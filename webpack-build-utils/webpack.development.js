ab319668b1a69f5a72cc29a99d46a728de4161c8
tolumide <tolumideshopein@gmail.com>
2022-04-03 20:32:55 +0100

    Add docker compose to the application to ease startup

--- webpack-build-utils/webpack.development.js
+++ webpack-build-utils/webpack.development.js
@@ -3,61 +3,61 @@ const path = require("path");
 require("dotenv").config();
 
 module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  devServer: {
-    historyApiFallback: {
-      disableRoot: true,
+    mode: "development",
+    devtool: "source-map",
+    devServer: {
+        host: "0.0.0.0",
+        hot: true,
+        allowedHosts: "all",
+        historyApiFallback: {
+            disableRoot: true,
+        },
+        port: 5050,
     },
-    port: 5050,
-    proxy: {
-      '/api': process.env.PROXY_URL,
-    }
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss|sass)/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              sourceMap: true,
-              modules: true,
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss|sass)/i,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 1,
+                            sourceMap: true,
+                            modules: true,
+                        },
+                    },
+                    // "postcss-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+                include: /\.module\.css$/,
             },
-          },
-          // "postcss-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
+            {
+                test: /\.(css|scss|sass)/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    // "postcss-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+                exclude: /\.module\.css$/,
             },
-          },
         ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.(css|scss|sass)/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          // "postcss-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-        exclude: /\.module\.css$/,
-      },
-    ],
-  },
+    },
 };
